036903b56793abeeb597f9fea6c6c9a9f26a2d88
luoweilwx <114204225+luoweilwx@users.noreply.github.com>
2022-09-23 11:55:40 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,9 @@
 public class Test{
     public static void main(String[] args){
           System.out.println(args[0]);
-          System.out.println("Second Version");
+          prinStr("Chris!");
     }
+    
+    public static void prinStr(String str) {
+        System.out.println("My Name = " + str");
 }
