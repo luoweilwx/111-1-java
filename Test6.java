bca6171a6d4a28b138c568459acc340799c15f65
luoweilwx <114204225+luoweilwx@users.noreply.github.com>
2022-10-07 10:32:15 +0800

    防呆

---
+++ Test6.java
@@ -0,0 +1,33 @@
+public class Test5{
+    public static void main(String[] args){
+		int grade;
+		
+		Scanner input = new Scanner(System);
+        System.out.println("Enter first integer:");
+        garde = input.nextInt();
+		 
+
+		if (gared > 100 & gared < 0){
+			System.out.println("Error");	
+		}
+		else{ 
+		Swith (grade / 10){
+			case 9:
+			case 10:
+			  System.out.println("A");
+			  break;
+			case 8:
+			  System.out.println("B");
+			  break;
+			case 7:
+			  System.out.println("C");
+			  break;
+			case 6:
+			  System.out.println("D");
+			  break;
+			default:
+			  System.out.println("E");
+			  break;
+		}
+    }
+}
