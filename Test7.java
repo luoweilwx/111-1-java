1c866b3da4c02af6da80357348d4b3a8be6dcb21
luoweilwx <114204225+luoweilwx@users.noreply.github.com>
2022-10-07 10:58:52 +0800

    Add files via upload

---
+++ Test7.java
@@ -0,0 +1,12 @@
+public class Test7{
+    public static void main(String[] args){
+		int grade = 9;
+		
+		while (grade < 10) {
+        System.out.println("%d %n" , grade);
+        grade = grade - 1
+		 
+
+		}
+    }
+}
