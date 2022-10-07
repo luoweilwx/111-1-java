105842183f825518a66fc2578c742839ffa90284
luoweilwx <114204225+luoweilwx@users.noreply.github.com>
2022-10-07 09:24:09 +0800

    Update Test4.java

--- Test4.java
+++ Test4.java
@@ -6,8 +6,22 @@ public class Test4{
         System.out.println("Enter first integer:");
         garde = input.nextInt();
 		  
-		if (garde >= 90 & garde <= 100);
+		if (garde >= 90 & garde <= 100); {
 			  System.out.println("A");
+		}
+		else if (garde >= 80 & garde <= 89); {
+			  System.out.println("B");
+		}
+		else if (garde >= 70 & garde <= 79); {
+			  System.out.println("C");
+		}
+		
+		else if (garde >= 60 & garde <= 69); {
+			  System.out.println("D");
+		}
+	    
+	    	else {
+			  System.out.println("E");
 		}
     }
 }
