f379f44b8b9fedc4a9f449edc3cbc6a5699833b5
luoweilwx <114204225+luoweilwx@users.noreply.github.com>
2022-10-07 08:59:28 +0800

    Add files via upload

---
+++ Test5.java
@@ -0,0 +1,28 @@
+public class Test5{
+    public static void main(String[] args){
+		int grade;
+		
+		Scanner input = new Scanner(System);
+        System.out.println("Enter first integer:");
+        garde = input.nextInt();
+		  
+		Swith (grade / 10){
+			case 9:
+			case 10:
+			  System.out.println("A");
+			  break;
+			case 8:
+			  System.out.println("B");
+			  break;
+			case 7:
+			  System.out.println("C");
+			  break;
+			case 6:
+			  System.out.println("D");
+			  break;
+			default:
+			  System.out.println("E");
+			  break;
+		}
+    }
+}
