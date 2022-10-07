810baa65296a49045725fd126b7ce423b31c6875
luoweilwx <114204225+luoweilwx@users.noreply.github.com>
2022-10-07 08:44:04 +0800

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,13 @@
+public class Test4{
+    public static void main(String[] args){
+		int grade;
+		
+		Scanner input = new Scanner(System);
+        System.out.println("Enter first integer:");
+        garde = input.nextInt();
+		  
+		if (garde >= 90 & garde <= 100);
+			  System.out.println("A");
+		}
+    }
+}
