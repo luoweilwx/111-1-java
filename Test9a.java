7a7eacf8b9a6d0d490356b44912be064e0492198
luoweilwx <114204225+luoweilwx@users.noreply.github.com>
2022-11-18 11:49:30 +0800

    Add files via upload

---
+++ Test9a.java
@@ -0,0 +1,20 @@
+public class Test9{
+    public static void main(String[] args){
+		int a;
+		int k = 7;
+		
+		System.out.printf("%d%n", k);
+		
+		test11 t10 = new test11();
+		a = t10.sub(k, 4);
+		System.out.printf("%d%n",a);
+	}
+}
+
+public class Test10{
+    public static void main(String[] args){
+		// int t = add(x,y);
+		int t = x + y;
+		return t;
+	}
+}	
